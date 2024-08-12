e64b694ff6e515c6cbdc90c5fb4e2aa7037216c0
nsisthan@gmail.com <162254547+evenatthaphan@users.noreply.github.com>
2024-08-12 14:15:13 +0700

    update buylotto

--- lib/pages/User/BuyLotto.dart
+++ lib/pages/User/BuyLotto.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-<<<<<<< Updated upstream
 class BuylottoPage extends StatefulWidget {
   const BuylottoPage({super.key});
 
@@ -9,16 +8,6 @@ class BuylottoPage extends StatefulWidget {
 }
 
 class _BuylottoPageState extends State<BuylottoPage> {
-=======
-class _BuylottoPagState extends StatefulWidget {
-  const _BuylottoPagState({super.key});
-
-  @override
-  State<_BuylottoPagState> createState() => __BuylottoPagStateState();
-}
-
-class __BuylottoPagStateState extends State<_BuylottoPagState> {
->>>>>>> Stashed changes
   @override
   Widget build(BuildContext context) {
     return Container();
